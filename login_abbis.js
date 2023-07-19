6464f22ce5b7989392cf11c85ac2840a3264929a
PropreryCientificGudes <127574320+AbbysTheWorld@users.noreply.github.com>
2023-07-19 16:20:46 -0300

    Update login_abbis.js

--- login_abbis.js
+++ login_abbis.js
@@ -157,3 +157,26 @@ class Login{
 }
 
 //export {Login};
+/* Exemplo da api usada como teste:
+var http = require('http')
+var url = require('url')   
+http.createServer(function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin','*')
+  res.writeHead(200, { 'Content-Type':'application/json' });
+
+let parametros=url.parse(req.url,true);
+
+let mat=parametros.query.matricula;
+let pas=parametros.query.senha;
+
+let dados=null
+
+if(mat=="123" && pas=="321"){
+  dados = {
+    nome:"Abbis",
+    acesso:10
+  }
+}
+
+  res.end(JSON.stringify(dados));
+}).listen(8080);*/
